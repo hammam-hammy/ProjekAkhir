2615176649fdc31640f6d43951e7d90e9b11290c
hammam-hammy <commsq17@gmail.com>
2024-07-08 09:35:40 -0700

    Push Project Akhir

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+ 
+     ©  {{date('Y')}} Portal Absensi Filkom <span class="d-none d-sm-inline-block"> - </i> Projek Akhir Mata Kuliah Pemrograman Web Lanjut</span>.
+</footer>
